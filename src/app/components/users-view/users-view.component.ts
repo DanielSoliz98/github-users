55bef16d8cb87ae14656dd75d10ee315ee8e0020
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-09-20 11:23:53 -0400

    nav bar and search input implemented

--- src/app/components/users-view/users-view.component.ts
+++ src/app/components/users-view/users-view.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-users-view',
-  templateUrl: './users-view.component.html',
-  styleUrls: ['./users-view.component.css']
+  selector: "app-users-view",
+  templateUrl: "./users-view.component.html",
+  styleUrls: ["./users-view.component.css"],
 })
 export class UsersViewComponent implements OnInit {
+  searchForm = new FormGroup({
+    userInput: new FormControl("", Validators.required),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.log(this.user.value);
   }
-
+  get user(): AbstractControl {
+    return this.searchForm.get("userInput");
+  }
 }
