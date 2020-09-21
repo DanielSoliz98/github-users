9ddcbd2c808dbfd9c94b245303f76eaec9d4c1d4
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-09-21 18:28:50 -0400

    repository model implemented

---
+++ src/app/models/repository.ts
@@ -0,0 +1,10 @@
+export interface Repository{
+    id: number,
+    name: string,
+    html_url: string,
+    description: string,
+    issues_url: string,
+    language: string,
+    open_issues_count: number,
+    forks_count: number
+}
