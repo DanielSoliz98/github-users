a5c2c05d7670ada856390d54035aba44c08e7395
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-09-21 18:27:39 -0400

    user service updated

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,16 +1,28 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { SEARCH_USERS } from '../constants';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Repository } from "src/app/models/repository";
+import { SEARCH_REPOS, SEARCH_USERS, SECRET_KEY } from "../constants";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  getUsers(username: string, page: number): Observable<any> {
+    return this.http.get(`${SEARCH_USERS}${username}&per_page=4&page=${page}&${SECRET_KEY}`);
+  }
 
-  getUsers(username: string, page: number): Observable<any>{
-    return this.http.get(`${SEARCH_USERS}${username}&per_page=4&page=${page}`);
+  getUser(username: string) {
+    return this.http.get(`${SEARCH_REPOS}${username}?${SECRET_KEY}`);
+  }
+
+  getRepositories(url: string, page: number): Observable<Repository[]> {
+    return this.http.get<Repository[]>(`${url}?per_page=4&page=${page}${SECRET_KEY}`);
+  }
+
+  getIssues(url: string): Observable<any[]> {
+    return this.http.get<any[]>(`${url}?${SECRET_KEY}`);
   }
 }
