1b1a8763057c1009698d2ee2f504ec1ca3eb0fc9
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-09-21 18:28:41 -0400

    new constant added

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -2,3 +2,4 @@ export const API_URL = "https://api.github.com/";
 export const SEARCH_USERS = API_URL + "search/users?q=";
 export const SEARCH_REPOS = API_URL + "users/"
 export const REPO_ISSUES = API_URL + "repos/";
+export const SECRET_KEY = "client_id=DanielSoliz98&client_secret=c917a1e4db9e13b9c14747c5d5ce2416e736281d"
