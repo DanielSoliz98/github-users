3c6c16689ae11bcb3621732fee8fcf7e939f6964
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-09-21 18:28:30 -0400

    new components added to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { UsersViewComponent } from './components/users-view/users-view.component';
 import { UserDetailCardComponent } from './components/user-detail-card/user-detail-card.component';
+import { RespositoriesViewComponent } from './components/respositories-view/respositories-view.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RepoDetailCardComponent } from './components/repo-detail-card/repo-detail-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersViewComponent,
-    UserDetailCardComponent
+    UserDetailCardComponent,
+    RespositoriesViewComponent,
+    NavBarComponent,
+    RepoDetailCardComponent
   ],
   imports: [
     BrowserModule,
