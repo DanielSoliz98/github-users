b100b8360b7995ec229b84b660340a5fd0c13592
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-09-20 23:52:53 -0400

    load more users function implemented

--- src/app/components/users-view/users-view.component.ts
+++ src/app/components/users-view/users-view.component.ts
@@ -19,19 +19,35 @@ export class UsersViewComponent implements OnInit {
   });
   users = [];
   username: string = "";
-  page: number = 1;
+  actualPage: number = 1;
+  pages: number = 0;
+  totalUsers: number = 0;
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {}
 
   onSubmit(): void {
+    this.actualPage = 1;
     this.username = this.user.value;
     this.user.setValue("");
     this.users = [];
-    this.userService.getUsers(this.username, this.page).subscribe((data) => {
-      this.users.push(data.items as User[]);
-    });
+    this.userService
+      .getUsers(this.username, this.actualPage)
+      .subscribe((data) => {
+        this.totalUsers = data.total_count;
+        this.pages = Math.round(data.total_count / 4);
+        this.users.push(data.items as User[]);
+      });
+  }
+
+  loadUsers() {
+    this.userService
+      .getUsers(this.username, this.actualPage + 1)
+      .subscribe((data) => {
+        this.actualPage++;
+        this.users.push(data.items as User[]);
+      });
   }
 
   get user(): AbstractControl {
