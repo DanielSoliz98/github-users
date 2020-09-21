912b932e8aede3a1db3dac23ec8c58072a40400f
Daniel Soliz Vargas <danielsoliz80@gmail.com>
2020-09-21 19:25:25 -0400

    user service modified for cache function

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,28 +1,46 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Repository } from "src/app/models/repository";
+import { Observable, of } from "rxjs";
+import { CacheService } from "../cache/cache.service";
 import { SEARCH_REPOS, SEARCH_USERS, SECRET_KEY } from "../constants";
+import { switchMap } from "rxjs/operators";
+import { Repository } from 'src/app/models/repository';
 
 @Injectable({
   providedIn: "root",
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cacheService: CacheService) {}
 
   getUsers(username: string, page: number): Observable<any> {
-    return this.http.get(`${SEARCH_USERS}${username}&per_page=4&page=${page}&${SECRET_KEY}`);
+    return this.httpCall(
+      `${SEARCH_USERS}${username}&per_page=4&page=${page}&${SECRET_KEY}`
+    );
   }
 
-  getUser(username: string) {
-    return this.http.get(`${SEARCH_REPOS}${username}?${SECRET_KEY}`);
+  getUser<T>(username: string): Observable<T> {
+    return this.httpCall(`${SEARCH_REPOS}${username}?${SECRET_KEY}`);
   }
 
   getRepositories(url: string, page: number): Observable<Repository[]> {
-    return this.http.get<Repository[]>(`${url}?per_page=4&page=${page}${SECRET_KEY}`);
+    return this.httpCall(`${url}?per_page=4&page=${page}&${SECRET_KEY}`);
   }
 
   getIssues(url: string): Observable<any[]> {
-    return this.http.get<any[]>(`${url}?${SECRET_KEY}`);
+    return this.httpCall(`${url}?${SECRET_KEY}`);
+  }
+
+  private httpCall<T>(url: string): Observable<T> {
+    const data = this.cacheService.load(url);
+    if (data !== null) {
+      return of<T>(data);
+    }
+
+    return this.http.request<T>("GET", url).pipe(
+      switchMap((response) => {
+        this.cacheService.save(url, response);
+        return of<T>(response);
+      })
+    );
   }
 }
